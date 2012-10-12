3fdde80a384e9083d509634bf6c8c9460e94af7f
root <root@Javito.LinuxPC>
2012-10-12 23:37:13 +0100

    Práctica 2

--- test.rb
+++ test.rb
@@ -1,1 +1,17 @@
-puts "ab"
+require "p2"
+require "test/unit"
+
+class TestPractica < Test::Unit::TestCase
+     def test1
+           assert_in_delta(12, Circulo.new('32.14'))
+     end
+
+     def test_typecheck
+       assert_raise( RuntimeError ) { Circulo.new('lpp') }
+     end
+
+     def test_failure
+           assert_equal(56, Circulo.new('32.14'))
+     end
+
+end
