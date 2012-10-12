6fc4385ea9cf5c99b88e9900d957d3f847c76b77
root <root@Javito.LinuxPC>
2012-10-12 21:43:17 +0100

    Codigo casi acabado

--- p2.rb
+++ p2.rb
@@ -1,1 +1,18 @@
+#! /usr/bin/env ruby
+class Circulo
+     def initialize(perimetro)
+	@perimetro = perimetro
+     end
+
+     def HallaRadio
+	return @perimetro * "3.14"
+     end
+end
+
+
 puts "Introduzca el perímetro de la circunferencia: "
+p = gets.chomp
+c = Circulo.new(p)
+puts "El radio de la circunferencia es: "
+c.HallaRadio
+sleep 5
